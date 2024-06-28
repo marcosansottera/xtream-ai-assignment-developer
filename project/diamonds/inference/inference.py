ee29d524b49de562f029ea9d288b862b9d6f17e8
Marco Sansottera <marco.sansottera@gmail.com>
2024-06-28 11:16:03 +0200

    feat: add class for inference

---
+++ project/diamonds/inference/inference.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Union
+
+import joblib
+
+from diamonds.entrypoints.data_generation.do_preprocess_data import (
+    BasicDataPreprocess,
+    CategoricalDataPreprocess,
+)
+
+
+class InferencePipeline:
+    def __init__(self, data_processor: Union[BasicDataPreprocess, CategoricalDataPreprocess], model_path: Path):
+        self.data_processor = data_processor
+        self.model_path = model_path
+        self.pipeline = None
+
+    def load_model(self):
+        self.pipeline = joblib.load(self.model_path)
+
+    def prepare_data(self, data):
+        processed_data = self.data_processor.postprocess(data)
+        return processed_data
+
+    def predict(self, data):
+        prepared_data = self.prepare_data(data)
+        predictions = self.pipeline.predict(prepared_data)
+        return predictions
